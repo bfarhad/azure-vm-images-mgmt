01c3bfa7fe79cea3fe6b533ae5419f9f8047eb6d
Farhad Badalov <fbadalov@pmintl.net>
2025-12-25 22:06:46 +0200

    Added modifications to tf, pipelines and readme file

--- terrafrom/modules/image-builder/imgbuilder.tf
+++ terrafrom/modules/image-builder/imgbuilder.tf
@@ -23,21 +23,3 @@ resource "azurerm_shared_image" "image" {
   tags = var.tags
 }
 
-resource "azurerm_shared_image_version" "version" {
-  name                = "1.0.0"
-  gallery_name        = azurerm_shared_image_gallery.gallery.name
-  image_name          = azurerm_shared_image.image.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-
-  target_region {
-    name                   = var.location
-    regional_replica_count = 1
-  }
-
-  # Note: For actual image building, use managed_image_id from a captured VM
-  # This is a placeholder for versioning structure
-  managed_image_id = null  # Replace with actual managed image ID
-
-  tags = var.tags
-}
