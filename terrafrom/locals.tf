9bbfa381a3011e71f6cb3051ff839daec8610491
Farhad Badalov <fbadalov@pmintl.net>
2025-12-25 19:36:39 +0200

    Fixing pipeline for retrieving values for vars-2

--- terrafrom/locals.tf
+++ terrafrom/locals.tf
@@ -1,7 +1,7 @@
 locals {
   resource_group_name     = "${var.environment}-${var.project_name}-rg"
   vnet_name               = "${var.environment}-${var.project_name}-vnet"
-  keyvault_name           = "${var.environment}-${var.project_name}-kv"
+  keyvault_name           = substr(replace("${var.environment}${var.project_name}kv", "-", ""), 0, 24)
   log_analytics_workspace = "${var.environment}-${var.project_name}-law"
-  gallery_name            = "${local.resource_group_name}-gallery"
+  gallery_name            = substr(replace("${local.resource_group_name}gallery", "-", ""), 0, 24)
 }
