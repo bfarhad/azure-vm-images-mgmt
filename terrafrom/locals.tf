7a5a580ffeed11f697e4708f4d79497749f10cc4
Farhad Badalov <fbadalov@pmintl.net>
2025-12-25 21:49:12 +0200

    Terraform code modification, need synatx check-6

--- terrafrom/locals.tf
+++ terrafrom/locals.tf
@@ -1,7 +1,7 @@
 locals {
   resource_group_name     = "rg-${var.environment}-${var.project_name}"
   vnet_name               = "${var.environment}-${var.project_name}-vnet"
-  keyvault_name           = substr(replace("${var.environment}${var.project_name}-kv", "-", ""), 0, 24)
+  keyvault_name           = "${var.environment}${var.project_name}-kv"
   log_analytics_workspace = "${var.environment}-${var.project_name}-law"
   gallery_name            = substr(replace("${local.resource_group_name}-gallery", "-", ""), 0, 24)
 }
