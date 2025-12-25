93bb8fcb7cfe2bbf75aad5d023919f9a4e7bebb5
Farhad Badalov <fbadalov@pmintl.net>
2025-12-26 01:57:16 +0200

    unchecked code, removed vmss, added vm for generalization

--- terrafrom/modules/security/security.tf
+++ terrafrom/modules/security/security.tf
@@ -52,6 +52,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowTomcat"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_key_vault" "kv" {
